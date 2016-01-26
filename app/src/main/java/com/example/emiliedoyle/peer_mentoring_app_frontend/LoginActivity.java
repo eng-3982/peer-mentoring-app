367740e25c4ccdf68bf84081f9fb130b43b1ba9d
Emilie Doyle <ecdoyle@users.noreply.github.com>
2016-01-26 13:16:37 -0500

    Update LoginActivity.java

--- app/src/main/java/com/example/emiliedoyle/peer_mentoring_app_frontend/LoginActivity.java
+++ app/src/main/java/com/example/emiliedoyle/peer_mentoring_app_frontend/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.emiliedoyle.peer_mentoring_app_frontend;
+package com.example.emiliedoyle.peer_mentoring_app;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
